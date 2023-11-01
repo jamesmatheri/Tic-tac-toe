c3ab9d2d88888606ded0eb8e896de867d411b902
unknown <jamesmkubwa18@gmail.com>
2023-11-01 04:35:41 +0300

    create iife function and facory functions that creates users and renders game board

--- app.js
+++ app.js
@@ -1,3 +1,68 @@
+const Gameboard = (() => {
+    let gameboard = ["","","","","","","","",""];
+
+    const render = () =>{
+       let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class= "square" id= "square-${index}">${square}</div>`
+        })
+
+        document.querySelector('.game-board').innerHTML = boardHTML;
+    }
+        return {
+            render,
+        }
+    
+})();
+
+
+const createPlayer = (name,mark)=>{
+ return{
+        mark,
+        name
+
+ }
+}
+
+const Game = (()=>{
+        let players = [];
+        let currentPlayerIndex;
+        let gameOver;
+        const start = () =>{
+            players= [
+                createPlayer(document.querySelector('#o').id,'o'),
+                createPlayer(document.querySelector('#x').id,'x')
+            ]
+            currentPlayerIndex =0;
+            gameOver = false;
+            Gameboard.render();
+        }
+        return {
+            start,
+        }
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const userButton_btn = document.querySelectorAll('.user-sign button');
 const pressStart_btn = document.querySelector('.press-start');
 const startGame_div = document.querySelector('.start-game');
@@ -7,7 +72,7 @@ Array.from(userButton_btn).forEach(function(btn){
     btn.addEventListener('click', event =>{
         event.preventDefault();
         btn.style.backgroundColor = "pink"
-       newPlayer(event.id);
+
        
        
     })
@@ -18,6 +83,7 @@ pressStart_btn.addEventListener('click',function(event){
     pressStart_btn.style.display = 'none';
     setTimeout(function(){
         gameContainer_div.style.display = 'flex'
+        Game.start();
     },1500)
 
 })
